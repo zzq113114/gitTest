d8f143e9e422205420e3e9fb8c8c6bb1754d6806
zhang <25793004402qq.com>
2021-02-06 01:22:26 +0800

    123123

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,11 @@
 
 public class Test {
 
-<<<<<<< HEAD
+
 	// wi234
 	// w 12123
 	// w123ter 4
    //  ma123er 5
 
-=======
-	// win 1
-	// win 2
-	// win 3
-	// master 4
-	//  master 5
->>>>>>> refs/remotes/origin/10224
+
 }
