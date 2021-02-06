4fe8967882096dd80c1569f6de59fec3d1ed73e2
zhang <25793004402qq.com>
2021-02-06 16:04:44 +0800

    --

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,27 @@
 
 public class Test {
 
-	// win 1
-	// win 12312
 
-	// win 3
-	// master 4
-   //  master 5
+	public static void main (String[] args){
+	int beernum =99;
+	String word = "bottle";
+	while (beernum>0){
+	if (beernum == 1){
+	word = "bootle";
+	}
 
+	System.out.print(beernum+""+word+"of beer on the wall");
+	System.out.println(beernum+""+"of beer");
+	System.out.println("Take one down.");
+	System.out.println("passit around.");
+	beernum = beernum -1;
 
+	   if (beernum>0); {
+	     System.out.println(beernum+""+"of beer on wall");
+	   }
+	   {
+	     System.out.println("No more bottles of beer on the wall");
+	   }
+	}
+}
 }
